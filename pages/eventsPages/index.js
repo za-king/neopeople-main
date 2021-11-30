dc9dbc36b9ad818a09ebb643fdbcf13ecf188a89
za-king <muhammadzakysyukur@gmail.com>
2021-11-30 11:14:12 -0800

    Update index.js

--- pages/eventsPages/index.js
+++ pages/eventsPages/index.js
@@ -1,11 +1,14 @@
-import Cards from "../../src/components/Cards"
-import Layout from "../../src/components/Layout"
+import Cards from "../../src/components/Cards";
+import Layout from "../../src/components/Layout";
 
 export default function events() {
-    return (
-        <Layout>
-            <Cards />
-            helo event
-        </Layout>
-    )
+  return (
+    <Layout>
+      <div className="flex justify-center p-10">
+        <h1 className="text-xl">EVENT</h1>
+      </div>
+
+      <Cards />
+    </Layout>
+  );
 }
