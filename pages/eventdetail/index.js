f3b60fc69a249c46a291cca783e14cedca189d05
za-king <muhammadzakysyukur@gmail.com>
2021-11-30 11:25:56 -0800

    Create index.js

---
+++ pages/eventdetail/index.js
@@ -0,0 +1,11 @@
+import EventDetail from '../../src/components/EventDetailComp'
+import Layout from "../../src/components/Layout";
+
+
+export default function eventdetail() {
+    return (
+        <Layout>
+            <EventDetail />
+        </Layout>
+    )
+}
