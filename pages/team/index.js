b78335c9a337a973c931ffac35ba2cf9340e1233
za-king <muhammadzakysyukur@gmail.com>
2021-11-30 11:08:28 -0800

    Update index.js

--- pages/team/index.js
+++ pages/team/index.js
@@ -1,9 +1,10 @@
 import Layout from "../../src/components/Layout"
+import Profile from "../../src/components/Profile"
 
 export default function team() {
     return (
         <Layout>
-            team
+            <Profile />
         </Layout>
     )
 }
