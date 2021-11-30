c5b93b8cfdc89dc6be0660be6f407a87455c18a8
za-king <muhammadzakysyukur@gmail.com>
2021-11-30 12:42:46 -0800

    Update index.js

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,9 +1,12 @@
-import Layout from "../../src/components/Layout"
-import FaqComp from '../../src/components/FaqComp'
+import Layout from "../../src/components/Layout";
+import FaqComp from "../../src/components/FaqComp";
 export default function faq() {
-    return (
-        <Layout>
-            <FaqComp />
-        </Layout>
-    )
+  return (
+    <Layout>
+      <div >
+        <h1 className='text-4xl font-sans font-thin'>F.A.Q</h1>
+      </div>
+      <FaqComp />
+    </Layout>
+  );
 }
