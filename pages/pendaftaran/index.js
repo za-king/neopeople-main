a96c6bd44dff5602d5d15517968808d54c80f4a1
za-king <muhammadzakysyukur@gmail.com>
2021-11-30 11:25:53 -0800

    Create index.js

---
+++ pages/pendaftaran/index.js
@@ -0,0 +1,10 @@
+import PendaftaranComp from "../../src/components/PendaftaranComp"
+import Layout from "../../src/components/Layout";
+
+export default function pendaftaran() {
+    return (
+        <Layout>
+            <PendaftaranComp />
+        </Layout>
+    )
+}
