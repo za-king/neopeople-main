94d2d7e2d4f9836c615d547f8e87b68ce1b805ce
za-king <muhammadzakysyukur@gmail.com>
2021-11-30 11:25:59 -0800

    Create index.js

---
+++ pages/buktipembayaran/index.js
@@ -0,0 +1,11 @@
+import BuktiPembComp from "../../src/components/BuktiPembComp"
+import Layout from "../../src/components/Layout";
+
+export default function buktipembayaran() {
+    
+    return (
+        <Layout>
+            <BuktiPembComp/>
+        </Layout>
+    )
+}
