a4b6722483860a43b583f395232a07c7f92b76f3
Ridho Muhammad <5mblackburn7@gmail.com>
2021-12-01 10:18:08 +0700

    fix style

--- src/components/LayananPage/card/index.js
+++ src/components/LayananPage/card/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import Image from 'next/dist/client/image';
 import { GiMedallist } from 'react-icons/gi';
 import { FaLaptopMedical } from 'react-icons/fa'
 import { SiBookstack } from 'react-icons/si'
 
-export default function LayananCard() {
+
+export default function LayananCard({cardData}) {
+    const cardStyle= "bg-[#F4F4F4] w-[315px] drop-shadow-2xl h-[494px] rounded flex justify-around items-center flex-col pt-4 px-8"
     return (
-        <div className=" bg-[#F4F8FE] h-[60vh] justify-center flex flex-col items-center" >
+        <div className={cardStyle}>
+        <div className="font-sans font-semibold text-2xl">
+            {cardData[0]}
         </div>
+        <div>
+            <Image src={cardData[1]}>
+            </Image>
+        </div>
+        <div className="min-h-[169px]"> 
+            <ul className="inline-flex justify-end items-start flex-col">
+                <li>{cardData[2][0]}</li>
+                <li>{cardData[2][1]}</li>
+                <li>{cardData[2][2]}</li>
+                <li>{cardData[2][3]}</li> 
+            </ul>
+        </div>
+        <div>
+            <div className="font-sans font-semibold text-3xl">
+                {cardData[3]}
+            </div>
+        </div>
+        <button className="rounded border-2 border-[#FF5800] border-solid w-[239px] h-[34px] bg-[#FCFCFC]">
+            Daftar Sekarang
+        </button>
+    </div>
+
     )
 }
--- src/components/LayananPage/index.js
+++ src/components/LayananPage/index.js
@@ -1,55 +1,58 @@
 import React from 'react'
-import Image from 'next/dist/client/image';
-import { GiMedallist } from 'react-icons/gi';
-import { FaLaptopMedical } from 'react-icons/fa'
-import { SiBookstack } from 'react-icons/si'
+import LayananCard from './card/';
 import EventIcon from '../../../public/icons/image 24.svg'
+import KonselingIcon from '../../../public/icons/image 25.svg'
+import KaryaIcon from '../../../public/icons/image 27.svg'
+
 
 
 export default function LayananPage() {
-    const cardStyle= "bg-[#F4F4F4] w-[315px] drop-shadow-2xl h-[494px] rounded flex justify-center items-center flex-col"
-    return (
-        <div className=" bg-[#F4F8FE] h-screen justify-center flex flex-col items-center" >
-            <div className="h-[25%] w-full flex justify-end items-center flex-col">
-                <div className="text-5xl font-sans font-thin">
+        const cardData = [
+            [
+                'Event',
+                EventIcon,
+                [
+                    'Kami mengadakan Webinar',
+                    'Workshop',
+                    'Sharing Session',
+                    'membahas mengenai kesehatan mental dan juga produktivitas'
+                ],
+                'Rp 30.000'
+            ],
+            [
+                'Konseling',
+                KonselingIcon,
+                [
+                    'Kamu dapat Konsultasi dengan Mentor Neo People',
+                    'Psikolog yang sudah mempunyai Izin Praktek',
+                ],
+                'Rp 30.000'
+            ],
+            [
+                'Karya',
+                KaryaIcon,
+                [
+                    'Kamu dapat Berkarya dengan keterampilan yang kamu kuasai',
+ 
+                ],
+                'Rp 30.000'
+            ]
+        ]
+        return (
+        <div className=" bg-[#F4F8FE] h-[120vh] justify-center flex flex-col items-center" >
+            <div className="h-[20%] w-full flex justify-end items-center flex-col">
+                <div className="text-4xl font-sans font-thin">
                     LAYANAN
                 </div >
-                <div className="text-2xl font-sans font-thin">
+                <div className="text-xl font-sans font-thin">
                     Temukan layanan Satu Persen yang sesuai untuk kamu.
                 </div>
             </div>
-            <div className="h-[75%] w-full grid grid-cols-3 gap-8 justify-items-center pt-10">
-                <div className={cardStyle}>
-                    <div>
-                        Event
-                    </div>
-                    <div>
-                        <Image src={EventIcon}>
-                        </Image>
-                    </div>
-                    <div>
-                        <ul>
-                            <li>Kami mengadakan Webinar</li>
-                            <li>Workshop</li>
-                            <li>Sharing Session</li>
-                            <li>membahas mengenai kesehatan mental dan juga produktivitas</li> 
-                        </ul>
-                    </div>
-                    <div>
-                        <div>
-                            Rp. 13.000
-                        </div>
-                    </div>
-                    <button>
-                        Daftar Sekarang
-                    </button>
-                </div>
-                <div className={cardStyle}>
-
-                </div>
-                <div className={cardStyle}>
-
-                </div>
+            <div className="h-[80%] w-full grid grid-cols-3 gap-4 justify-items-center pt-10">
+                <LayananCard cardData = {cardData[0]}/>
+                <LayananCard cardData = {cardData[1]}/>
+                <LayananCard cardData = {cardData[2]}/>
+                
             </div>
         </div>
     )
