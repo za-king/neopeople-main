bc64c3181c03e1fcc4ff73055dec0eaffe547443
za-king <muhammadzakysyukur@gmail.com>
2021-11-30 22:14:05 -0800

    Update index.js

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,8 +1,9 @@
 
 export default function Sidebar() {
   return (
-    <nav className="absolute z-10 w-1/2 bg-indigo-900 text-white h-screen">
-        tes
-    </nav>
+        <div>
+          
+        </div>
+    
   );
 }
