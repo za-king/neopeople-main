17cbdc2992adde014ef7e8b8405ff9ab5b025fd8
za-king <muhammadzakysyukur@gmail.com>
2021-12-02 11:16:10 -0800

    Update index.js

--- src/components/Apa/index.js
+++ src/components/Apa/index.js
@@ -4,47 +4,23 @@ import { ImQuotesLeft, ImQuotesRight } from "react-icons/im";
 import Image from "next/dist/client/image";
 import Profile from "../../../public/images/img1.jpg";
 
-
+import SwiperComp from "../SwiperComp";
 
 export default function Apa() {
   return (
-    <div className=" flex bg-[#050C44] h-[95vh] justify-center">
-      <div className="m-[60px] flex justify-center flex-col items-start w-full">
-        <div className="text-4xl h-[10%] text-white font-thin">
-          Apa kata mereka?
-        </div>
-        <div className="text-2xl h-[20%] text-white font-thin w-[20%]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        </div>
-        <div className="w-[full] h-[50%] flex flex-row justify-center items-center">
-          <div className="bg-[#E4E4E4] h-full w-[75%] flex flex-row justify-center items-center rounded">
-            <div className="w-[30%] h-full bg-blue-200 relative overflow-hidden rounded">
-              <Image src={Profile} height={100} layout="fill" />
-            </div>
-            <div className="w-[70%] px-8 flex justify-center flex-col items-center">
-              <div className="flex w-full justify-start">
-                <ImQuotesLeft size={60} />
-              </div>
-              <div className="text-left w-[90%]">
-                Alhamdulillah setelah aku kenal Neo People ada perubahan hidup
-                pelan-pelan seperti sekarang agak tegar kalau menghadapi
-                masalah, terus lingkungan juga merasakan emosional aku lebih
-                stabil. Terima kasih yaa Neo People sudah membentuk komunitas
-                yang bermanfaat, semoga kita bisa selalu #tumbuhbersama
-              </div>
-              <div className="flex w-full justify-end">
-                <ImQuotesRight size={60} />
-              </div>
-            </div>
+    <>
+      <div className=" flex bg-[#050C44] h-[95vh] ">
+        <div className="m-[60px] flex justify-center flex-col items-start w-full">
+          <div className="text-4xl h-[10%] text-white font-thin">
+            Apa kata mereka?
           </div>
-          <div className="rounded-md bg-blue-300 p-4 absolute left-[12%]">
-            <GrPrevious size={35} />
+          <div className="text-2xl h-[20%] text-white font-thin w-[20%]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
           </div>
-          <div className="rounded-md bg-blue-300 p-4 absolute right-[12%]">
-            <GrNext size={35} />
-          </div>
+
+          <SwiperComp />
         </div>
       </div>
-    </div>
+    </>
   );
 }
