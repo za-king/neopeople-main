07bf83ff242c8d6b3a30f3dd0f66a60f550fa2d2
za-king <muhammadzakysyukur@gmail.com>
2021-12-04 16:12:36 -0800

    Update index.js

--- src/components/Attention/index.js
+++ src/components/Attention/index.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import Fade from "react-reveal/Fade";
 
 export default function Attention() {
     return (
         <div className=" flex bg-[#FF5800] h-[40vh] justify-center items-center flex-col" >
-            <div className="text-6xl font-sans font-thin">Attention</div>
-            <div className="text-xl font-sans font-thin text-center w-[60%]">                
+            <Fade bottom>
+            <div className="text-6xl font-sans font-thin">
+                Attention
+                </div>
+            </Fade>
+            
+                <Fade bottom>
+                <div className="text-xl font-sans font-thin text-center w-[60%]">                
                 Jika kamu merasakan gangguan psikologis yang cukup berat silahkan 
                 segera konsultasikan apa yang sedang kamu rasakan ke
                 tenaga profesional atau telfon ke 119 untuk panggilan darurat.
             </div>
+                </Fade>
+            
         </div>
     )
 }
