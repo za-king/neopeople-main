ed89659e9583ca9c926a02c011133f00549c7331
za-king <muhammadzakysyukur@gmail.com>
2021-12-30 23:17:26 +0700

    ok2

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,9 @@
 import Layout from '../../src/components/Layout'
-
+import BlogComp from '../../src/components/blogComp'
 export default function blog() {
     return (
         <Layout>
-            
+            <BlogComp />
         </Layout>
     )
 }
